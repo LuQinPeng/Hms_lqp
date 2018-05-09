0e06e6a793d7e20e72a67fc10088e33c4c056b98
Administrator <Administrator@MS-20180422RYKB>
2018-05-09 10:05:01 +0800

    first

---
+++ firstGit/src/aaa/a1.java
@@ -0,0 +1,9 @@
+package aaa;
+
+public class a1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("bcdsjnkjd");
+	}
+}
---
+++ firstGit/src/firstGit/dome.java
@@ -0,0 +1,14 @@
+package firstGit;
+
+public class dome {
+
+	public static void main(String[] args) {
+		System.out.println("我的项目");
+
+	}
+	public void add(){
+		System.out.println("dskcsd");
+		
+	}
+
+}
